8078b993c8bee652ad69f2d7d797c62e3d1945fa
Haavisto Jari T T <jtthaavi@da1-cs-b221-29.helsinki.fi>
2014-01-21 14:02:38 +0200

    perustoiminnallisuutta aloitettu

--- brewscale/src/main/java/brewscale/brewscale/App.java
+++ brewscale/src/main/java/brewscale/brewscale/App.java
@@ -7,9 +7,24 @@ package brewscale.brewscale;
 public class App 
 {
     public static void main( String[] args ) {
-        Resepti resepti = new Resepti("Testiresepti", 4.5);
-        Tyokalut tkl = new Tyokalut(resepti);
-        resepti = tkl.skaalaa(2);
-        System.out.println(resepti);
+        Resepti resepti = new Resepti("Tester Ale", 10);
+        Mallas m1 = new Mallas("Pale Ale", 1.2);
+        Mallas m2 = new Mallas("Crystal 100", 0.6);
+        Mallas m3 = new Mallas("Roast Barley", 0.1);
+        resepti.lisaaMallas(m1);
+        resepti.lisaaMallas(m2);
+        resepti.lisaaMallas(m3);
+        
+        Humala h1 = new Humala("Hallertau", 12, 5.5);
+        Humala h2 = new Humala("Fuggles", 6, 7.0);
+        resepti.lisaaHumala(h1);
+        resepti.lisaaHumala(h2);
+        
+        Aines a1 = new Aines("Sokeri", 0.5);
+        resepti.lisaaAines(a1);
+        
+        System.out.println(resepti.reseptiTeksti());
+        Brewscale b = new Brewscale(resepti);
+        System.out.println(b.skaalaa(2).reseptiTeksti());
     }
 }
